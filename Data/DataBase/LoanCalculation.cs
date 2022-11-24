4411b050b40053677a82d2791b4028cab0c02fc6
jikoso2 <jikoso2@o2.pl>
2022-11-24 12:16:47 +0100

    Add Loading application, walidation loan calculations, some refactor

--- Data/DataBase/LoanCalculation.cs
+++ Data/DataBase/LoanCalculation.cs
@@ -9,11 +9,17 @@ namespace MyFinances.Data.DataBase
 
 		public int iid { get; set; }
 
+		[Required]
+		[Range(1, 10000000, ErrorMessage = "Wysokość kredytu nie może być mniejsza od złotówki lub większa od 10 milionów złotych")]
 		public long amount { get; set; }
 
-		public int duration { get; set; }
-
+		[Required]
+		[Range(0.01, 30, ErrorMessage = "Wysokość oprocentowania nie może być ujemna, mniejsza od 0,01 % lub większa od 30 %")]
 		public double percentage { get; set; }
+
+		[Required]
+		[Range(12, 420, ErrorMessage = "Długość kredytu musi zawierać się w przedziale od 12 do 420 miesięcy")]
+		public int duration { get; set; }
 
 		public Guid fk_user_id { get; set; }
 	}
